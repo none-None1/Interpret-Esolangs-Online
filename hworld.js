6568f2c55f7f2af7c76d0d8eeba783717703c676
none_None1 <mxm20110612@163.com>
2023-08-28 11:48:50 +0800

    Added PlusOrMinus and HWorld

---
+++ hworld.js
@@ -0,0 +1,24 @@
+function hworld(progam,input){
+    if(program=='h'){
+        return 'Hello!';
+    }
+    if(program=='hh'){
+        return 'Hello, Hello!';
+    }
+    if(program=='w'){
+        return 'World!';
+    }
+    if(program=='ww'){
+        return 'World, World!';
+    }
+    if(program=='hw'){
+        return 'Hello, World!';
+    }
+    if(program=='wh'){
+        return 'World, Hello!';
+    }
+    if(program=='q'){
+        return '';
+    }
+    return program;
+}
---
+++ plusorminus.js
@@ -0,0 +1,11 @@
+function plusorminus(program,input){
+    var output='',x=0;
+    for(let i of program){
+        if(i=='+'){
+            x++;if(x==256){x=0}
+        }else{
+            output+=String.fromCharCode(x);x--;if(x==-1){x=255}
+        }
+    }
+    return output
+}
