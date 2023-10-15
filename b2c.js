eb631aef0360d27c0f30cf91b870ef3be445a46e
none_None1 <mxm20110612@163.com>
2023-10-15 21:56:04 +0800

    B2C support

---
+++ b2c.js
@@ -0,0 +1,71 @@
+function b2c(program,input){
+    var stack=[];
+    var matches={};
+    var ip=0;
+    var tape=[0,0];
+    var p=0;
+    var output='';
+    for(var i=0;i<program.length;i++){
+        if(program[i]=='['){
+            stack.push(i);
+        }
+        if(program[i]==']'){
+            if(stack.length==0){
+                throw new Error('Right bracket does not match left bracket');
+            }
+            var mt=stack.pop();
+            matches[mt]=i;
+            matches[i]=mt;
+        }
+    }
+    if(stack.length!=0){
+        throw new Error('Left bracket does not match right bracket');
+    }
+    while(ip<program.length){
+        if(program[ip]=='+'){
+            tape[p]=tape[p]+1;
+            if(tape[p]==256) tape[p]=0;
+            ip=ip+1;
+        }
+        if(program[ip]=='-'){
+            tape[p]=tape[p]-1;
+            if(tape[p]==-1) tape[p]=255;
+            ip=ip+1;
+        }
+        if(program[ip]=='|'){
+            p=1-p;
+            ip=ip+1;
+        }
+        if(program[ip]==','){
+            if(input==''){
+                tape[p]=0;
+            }else{
+                tape[p]=input.charCodeAt(0)%256;
+                input=input.slice(1);
+            }
+            ip=ip+1;
+        }
+        if(program[ip]=='.'){
+            output+=String.fromCharCode(tape[p]);
+            ip=ip+1;
+        }
+        if(program[ip]=='['){
+            if(tape[p]==0){
+                ip=matches[ip];
+            }else{
+                ip=ip+1;
+            }
+        }
+        if(program[ip]==']'){
+            if(tape[p]!=0){
+                ip=matches[ip];
+            }else{
+                ip=ip+1;
+            }
+        }
+        if(!('+-,.[]|'.includes(program[ip]))){
+            ip=ip+1;
+        }
+    }
+    return output;
+}
