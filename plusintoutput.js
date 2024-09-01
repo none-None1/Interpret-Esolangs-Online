47826d68b59dc20c85ceb3c47b8db449f2e7e9ce
none_None1 <mxm20110612@163.com>
2024-09-01 11:42:03 +0800

    Refactor code, add PlusIntOutput and PlusOrOutput support

---
+++ plusintoutput.js
@@ -0,0 +1,24 @@
+function plusintoutput(program){
+    var x=0,output='';
+    while(program.length){
+        if(program[0]=='+'){
+            var a=0;
+            program=program.slice(1);
+            if(!'0123456789'.includes(program[0])){
+                x=(x+1)%256;
+            }else{
+                while('0123456789'.includes(program[0])){
+                    a=a*10+program.charCodeAt(0)-48;
+                    program=program.slice(1);
+                }
+                x=(x+a)%256;
+            }
+        }else if(program[0]=='.'){
+            output+=String.fromCharCode(x);
+            program=program.slice(1);
+        }else{
+            program=program.slice(1);
+        }
+    }
+    return output;
+}
---
+++ plusoroutput.js
@@ -0,0 +1,11 @@
+function plusoroutput(program){
+    var output='',x=0;
+    for(let i of program){
+        if(i=='+'){
+            x++;if(x==256){x=0}
+        }else if(i=='-'){
+            output+=String.fromCharCode(x);
+        }
+    }
+    return output;
+}
