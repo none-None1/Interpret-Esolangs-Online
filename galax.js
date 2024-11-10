a0026392175ee8afdacf504437e38c4fe342c833
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-10 12:44:30 +0300

    Create galax.js

---
+++ galax.js
@@ -0,0 +1,12 @@
+//›*&«&^ is a esolang created by User:Ractangle with only three commands which are
+//>* Adds one and multiples by 2
+//<< decrements the value by two
+//^ outputs the ASCII character of the value
+function galax(program,input){
+    var output='';var acum=0;
+  for(var p=0;i<program.length;i++){
+    if (program[p]==">" and program[p+1]=="*"){acum++;acum=acum*2;p++;}
+    if (program[p]=="<" and program[p+1]=="<"){acum-=2;p++;}
+    if (program[p]=="^"){output+=String.fromCharCode(acum);};
+  };
+};
