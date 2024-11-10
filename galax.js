00d69110e716e8ecc839b56159ebc1781073fdd6
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-10 12:49:01 +0300

    Update galax.js

--- galax.js
+++ galax.js
@@ -4,7 +4,7 @@
 //^ outputs the ASCII character of the value
 function galax(program,input){
     var output='';var acum=0;
-  for(var p=0;i<program.length;i++){
+  for(var p=0;p<program.length;p++){
     if (program[p]==">",program[p+1]=="*"){acum++;acum=acum*2;p++;}
     if (program[p]=="<",program[p+1]=="<"){acum-=2;p++;}
     if (program[p]=="^"){output+=String.fromCharCode(acum);};
