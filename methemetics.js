82d31de3cfab83f16ca04bf714053e49d6c978e4
Win7HE <158824798+rttyg46305Unj@users.noreply.github.com>
2026-07-23 08:19:51 +0300

    Update methemetics.js

--- methemetics.js
+++ methemetics.js
@@ -1,6 +1,6 @@
 async function methemetics(program, input){
     let resp=await fetch('methemetics.py');
     var methemetics_py_code=await resp.text();
-    execute(methemetics_py_code,code,input,'methemetics');
+    execute(methemetics_py_code,code,input,'methemetics',['re']);
 }
 window.methemetics=methemetics;
